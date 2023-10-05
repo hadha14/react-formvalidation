aebc069186afff3830c56eb644caa2d5611e5c34
hadha14 <122806860+hadha14@users.noreply.github.com>
2023-10-05 15:06:17 +0530

    Add files via upload

---
+++ src/Home.js
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const Home = () => {
+    const usenavigate = useNavigate();
+    const [customerlist, listupdate] = useState(null);
+   
+    useEffect(() => {
+       
+
+        // let jwttoken = sessionStorage.getItem('jwttoken');
+        // fetch("https://localhost:44308/Customer", {
+        //     headers: {
+        //         'Authorization': 'bearer ' + jwttoken
+        //     }
+        // }).then((res) => {
+        //     return res.json();
+        // }).then((resp) => {
+        //     listupdate(resp);
+        // }).catch((err) => {
+        //     console.log(err.messsage)
+        // });
+
+    }, []);
+
+    return (
+        <div>
+            
+            <h1 className="text-center"> Zen Finance</h1>
+            {/* <table className="table table-bordered">
+                <thead>
+                    <tr>
+                        <td>Code</td>
+                        <td>Name</td>
+                        <td>Email</td>
+                        <td>Credit Limit</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {customerlist &&
+                        customerlist.map(item => (
+                            <tr key={item.id}>
+        <td>{item.id}</td>
+        <td>{item.name}</td>
+        <td>{item.email}</td>
+        <td>{item.creditLimit}</td>
+                            </tr>
+
+                        ))
+                    }
+                </tbody>
+
+            </table> */}
+            <img src="https://images.pexels.com/photos/53621/calculator-calculation-insurance-finance-53621.jpeg?auto=compress&cs=tinysrgb&w=400" alt="" style={{width: "100%"}}/>
+        </div>
+    );
+}
+
+export default Home;
